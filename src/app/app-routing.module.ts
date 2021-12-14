ce5dc744b7349473667d125af73b671fe8996f4a
Kevin Escobedo <Kevin@APEXSISTEMAS2021.COM>
2021-12-14 12:20:39 -0300

    Agregada ruta para componente vendedor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClienteComponent } from './components/cliente/cliente.component';
 import { FamiliaComponent } from './components/familia/familia.component';
+import { VendedorComponent } from './components/vendedor/vendedor.component';
 
 const routes: Routes = [
   { path: 'clientes', component: ClienteComponent , data: { title: 'Cliente' } },
   { path: 'familia', component: FamiliaComponent , data: { title: 'Familia' } },
+  { path: 'vendedor', component: VendedorComponent , data: { title: 'Vendedor' } },
 ];
 
 @NgModule({
