fea819493be152fd4247c31b6ac77555697dd9af
Kevin Escobedo <Kevin@APEXSISTEMAS2021.COM>
2021-12-14 11:33:03 -0300

    Agregada carpeta models para entidades

---
+++ src/app/models/cliente.ts
@@ -0,0 +1,11 @@
+export interface Cliente {
+    id_evt_Cliente:number
+    Cod_Cliente:String
+    Nombre_Cliente:String 
+    id_evt_Tipo_Documento:number 
+    NumeroDocumento_Cliente:String 
+    TipoIVA_Cliente:String
+    Mail_Inicio_Prod:String
+    Mail_Fin_Prod:String
+    id_evt_Vendedor:number
+}
---
+++ src/app/models/vendedor.ts
@@ -0,0 +1,7 @@
+export interface Vendedor {
+  id_evt_Vendedor: number;
+  Cod_Vendedor: String;
+  Nombre_Vendedor: String;
+  Celular_Vendedor: String;
+  Mail_Vendedor: String;
+}
